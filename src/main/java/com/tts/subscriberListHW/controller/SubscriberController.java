6c87a1c81a5a91207d0e1780e7ed35a7da4b541e
David Adams <david.riding.rails@gmail.com>
2021-02-14 11:37:34 -0700

    update SubscriberController

--- src/main/java/com/tts/subscriberListHW/controller/SubscriberController.java
+++ src/main/java/com/tts/subscriberListHW/controller/SubscriberController.java
@@ -1,15 +1,34 @@
 package com.tts.subscriberListHW.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tts.subscriberListHW.model.Subscriber;
+import com.tts.subscriberListHW.repository.SubscriberRepository;
 
 @Controller
 public class SubscriberController {
+  
+  @Autowired
+  private SubscriberRepository subscriberRepository;
 
-    @GetMapping(value = "/")
-    public String index(Subscriber subscriber) {
-      return "subscriber/index";
-    }
+  @GetMapping(value = "/")
+  public String index(Subscriber subscriber) {
+    return "subscriber/index";
+  }
+  
+  @PostMapping(value = "/")
+  public String addNewSubscriber(Subscriber subscriber, Model model) {
+    Subscriber subscriberToAdd = new Subscriber(subscriber.getFirstName(), 
+        subscriber.getLastName(),
+        subscriber.getUsername());
+    subscriberRepository.save(subscriberToAdd);
+    model.addAttribute("firstName", subscriberToAdd.getFirstName());
+    model.addAttribute("lastName", subscriberToAdd.getLastName());
+    model.addAttribute("username", subscriberToAdd.getUsername());
+    return "subscriber/result";
+  }
 }
