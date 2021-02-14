e7c36b1c452d00422487a034081de61e3761ca51
David Adams <david.riding.rails@gmail.com>
2021-02-14 11:19:33 -0700

    initial commit

---
+++ src/main/java/com/tts/subscriberListHW/SubscriberListHwApplication.java
@@ -0,0 +1,13 @@
+package com.tts.subscriberListHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberListHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriberListHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/subscriberListHW/SubscriberListHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.subscriberListHW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriberListHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
